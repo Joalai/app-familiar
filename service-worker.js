7e58073eac657f39d99eb26bc051c00033077a04
Joalai <borzo@outlook.com>
2026-09-09 07:58:13 +0200

    Actualizar caché para reparto de Inicio

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_PATCH_VERSION='2026.09.09.5';
+const APP_PATCH_VERSION='2026.09.09.6';
 
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
