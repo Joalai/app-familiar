4e28bd8f7a58675f9108e5febcaeb2e43366caa6
Joalai <borzo@outlook.com>
2026-09-09 08:08:56 +0200

    Actualizar caché para carga de Maleta

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_PATCH_VERSION='2026.09.09.6';
+const APP_PATCH_VERSION='2026.09.09.7';
 
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
