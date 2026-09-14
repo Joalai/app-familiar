b757a55113843d1fb0508ade58d549085c688986
Joalai <borzo@outlook.com>
2026-09-14 17:46:27 +0200

    Actualizar PWA a 2026.09.14.6

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_PATCH_VERSION='2026.09.14.5';
+const APP_PATCH_VERSION='2026.09.14.6';
 
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
