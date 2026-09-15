e98104c21a8ba03e06774e3b92ee2dd2f0972fc4
Joalai <borzo@outlook.com>
2026-09-15 10:36:02 +0200

    Release health appointment emphasis update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_PATCH_VERSION='2026.09.15.1';
+const APP_PATCH_VERSION='2026.09.15.2';
 
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
