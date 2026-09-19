1206fee927daa73f6ffa560be6c465982ef543e4
Joalai <borzo@outlook.com>
2026-09-19 23:34:20 +0200

    Actualizar versión a 2026.09.19.9

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_PATCH_VERSION='2026.09.19.8';
+const APP_PATCH_VERSION='2026.09.19.9';
 
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
