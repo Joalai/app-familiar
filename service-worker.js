af7fea199067ebd81fe6c620e3709a7349dca1db
Joalai <borzo@outlook.com>
2026-09-20 01:30:27 +0200

    Refresh document activation cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_PATCH_VERSION='2026.09.19.6';
+const APP_PATCH_VERSION='2026.09.19.7';
 
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
